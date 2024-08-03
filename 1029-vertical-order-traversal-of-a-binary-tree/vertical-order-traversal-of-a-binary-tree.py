ab56f655ffa43defd7b53e14306923a0fc92d716
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-08-03 19:33:26 +0530

    Time: 23 ms (99.66%) | Memory: 16.8 MB (46.72%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -1,36 +1,32 @@
-from collections import defaultdict, deque
-from typing import List, Optional
-
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import defaultdict,deque
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if not root:
-            return []
-        
-        node_list = []
-        
-        def bfs(root):
-            q = deque([(root, 0, 0)]) # node, vertical, horizontal
-            while q:
-                node, x, y = q.popleft()
-                if node:
-                    node_list.append((x, y, node.val))
-                    q.append((node.left, x - 1, y + 1))
-                    q.append((node.right, x + 1, y + 1))
-        
-        bfs(root)
-        
-        # Sort by x (vertical), then by y (horizontal), then by node value
-        node_list.sort()
-        
-        res = defaultdict(list)
-        for x, y, value in node_list:
-            res[x].append(value)
-        
+        if not root:return []
+        curr=root
+        q= deque()
+        nodes=[]
+        q.append((curr,0,0))
+
+        while q:
+            node,x,y=q.popleft()
+            nodes.append((x,y,node.val))
+            if node.left:
+                q.append((node.left,x-1,y+1))
+            if node.right:
+                q.append((node.right,x+1,y+1))
+        nodes.sort()
+
+        res=defaultdict(list)
+
+        for x,y,val in nodes:
+            res[x].append(val)
         return [res[x] for x in sorted(res)]
+
+
+        
