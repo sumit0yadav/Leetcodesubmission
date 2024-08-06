3a818d4a39a894d0a618a9cc238ff23b16a9fb15
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-08-06 19:36:18 +0530

    Time: 81 ms (64.10%) | Memory: 20.4 MB (79.52%) - LeetSync

--- 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.py
@@ -1,10 +1,10 @@
-# # Definition for a binary tree node.
-# # class TreeNode(object):
-# #     def __init__(self, x):
-# #         self.val = x
-# #         self.left = None
-# #         self.right = None
-# from collections import deque
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+from collections import deque
 # class Codec:
 
 #     def serialize(self, root):
@@ -22,18 +22,18 @@
 #             q.append(node)
 
 #             while q:
-#                 size=len(q)
-#                 for _ in range(size):
-#                     curr=q.popleft()
-#                     if curr:
-#                         l.append(curr.val)
-#                         q.append(curr.left)
-#                         q.append(curr.right)
-#                     else:
-#                         l.append('#')
-#             return ''.join(map(str, l))
-            
+                
+#                 curr=q.popleft()
+#                 if curr:
+#                     l.append(str(curr.val))
+#                     q.append(curr.left)
+#                     q.append(curr.right)
+#                 else:
+#                     l.append('#')
+#             print(l)
+#             return ''.join(l)
         
+#         return h(root)
 
 #     def deserialize(self, data):
 #         """Decodes your encoded data to tree.
@@ -41,7 +41,8 @@
 #         :type data: str
 #         :rtype: TreeNode
 #         """
-#         if len(data)==0:return None
+#         if len(data)==0:
+#             return None
 #         node=TreeNode(int(data[0]))
 #         curr=node
 #         q=deque()
@@ -53,34 +54,17 @@
 #             if data[i]!='#':
 #                 point.left=TreeNode(int(data[i]))
 #                 q.append(point.left)
-#             else:
-#                 point.left=None
+            
 #             i+=1
-#             if data[i]!='#':
+#             if i<len(data) and data[i]!='#':
+                
 #                 point.right=TreeNode(int(data[i]))
 #                 q.append(point.right)
-#             else:
-#                 point.right=None
+                
 #             i+=1
 #         return node
 
-
-        
-
-# # Your Codec object will be instantiated and called as such:
-# # ser = Codec()
-# # deser = Codec()
-# # ans = deser.deserialize(ser.serialize(root))
-from collections import deque
-
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Codec:
-
     def serialize(self, root):
         """Encodes a tree to a single string.
         
@@ -120,16 +104,25 @@ class Codec:
 
         while q and i < len(nodes):
             node = q.popleft()
-            if nodes[i] != "#":
-                node.left = TreeNode(int(nodes[i]))
+            
+            left_val = nodes[i]
+            if left_val != "#":
+                node.left = TreeNode(int(left_val))
                 q.append(node.left)
+            
             i += 1
-            if i < len(nodes) and nodes[i] != "#":
-                node.right = TreeNode(int(nodes[i]))
-                q.append(node.right)
+            if i < len(nodes):
+                right_val = nodes[i]
+                if right_val != "#":
+                    node.right = TreeNode(int(right_val))
+                    q.append(node.right)
+                
             i += 1
 
         return root
+
+
+        
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
