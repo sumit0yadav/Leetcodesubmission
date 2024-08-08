e7577172babdc9116d519de3bd40bb019189ed3b
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-08-08 13:08:35 +0530

    Time: 378 ms (12.74%) | Memory: 79.5 MB (7.35%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,35 @@
+
+
+import java.util.*;
+
+class Solution {
+static int getAns(int arr[], int n,  int ind, int prev_index,int[][] dp){
+    
+    // base condition
+    if(ind == n)
+        return 0;
+        
+    if(dp[ind][prev_index+1]!=-1)
+        return dp[ind][prev_index+1];
+    
+    int notTake = 0 + getAns(arr,n,ind+1,prev_index,dp);
+    
+    int take = 0;
+    
+    if(prev_index == -1 || arr[ind] > arr[prev_index]){
+        take = 1 + getAns(arr,n,ind+1,ind,dp);
+    }
+    
+    return dp[ind][prev_index+1] = Math.max(notTake,take);
+}
+
+public int lengthOfLIS(int[] arr){
+    int n=arr.length;
+    
+    int dp[][]=new int[n][n+1];
+    for(int row[]: dp)
+    Arrays.fill(row,-1);
+    
+    return getAns(arr,n,0,-1,dp);
+}
+}
