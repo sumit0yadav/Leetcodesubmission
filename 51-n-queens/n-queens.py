70b5db7d83d2b51b5f3fa96a860df0c85e9a9e2b
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-08-17 11:00:03 +0530

    Time: 90 ms (15.81%) | Memory: 16.9 MB (72.83%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,32 +1,38 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
 
-        res = []
-        nQueens = ['.' * n for _ in range(n)]
-        self.solveNQueensHelper(res, nQueens, 0, n)
-        return res
+        def solve(n):
+            res=[]
 
-    def solveNQueensHelper(self, res, nQueens, row, n):
-        if row == n:
-            res.append(list(nQueens))
-            return
-        for col in range(n):
-            if self.isValid(nQueens, row, col, n):
-                nQueens[row] = nQueens[row][:col] + 'Q' + nQueens[row][col+1:]
-                self.solveNQueensHelper(res, nQueens, row + 1, n)
-                nQueens[row] = nQueens[row][:col] + '.' + nQueens[row][col+1:]
+            nqueens=['.'* n for _ in range(n) ]
 
-    def isValid(self, nQueens, row, col, n):
-        # Check if the column had a queen before.
-        for i in range(row):
-            if nQueens[i][col] == 'Q':
-                return False
-        # Check if the 45° diagonal had a queen before.
-        for i, j in zip(range(row - 1, -1, -1), range(col - 1, -1, -1)):
-            if nQueens[i][j] == 'Q':
-                return False
-        # Check if the 135° diagonal had a queen before.
-        for i, j in zip(range(row - 1, -1, -1), range(col + 1, n)):
-            if nQueens[i][j] == 'Q':
-                return False
-        return True
+            
+
+            def solvehelper(res,nqueens,row,n):
+
+                if row==n:
+                    res.append(list(nqueens))
+                    return
+                for col in range(0,n):
+                    if valid(nqueens,row,col,n):
+                        nqueens[row]=nqueens[row][:col]+'Q'+nqueens[row][col+1:]
+                        solvehelper(res,nqueens,row+1,n)
+                        nqueens[row]=nqueens[row][:col]+'.'+nqueens[row][col+1:]
+            
+            def valid(nqueens,row,col,n):
+                for i in range(row):
+                    if nqueens[i][col]=='Q':
+                        return False
+                for i,j in zip(range(row-1,-1,-1),range(col-1,-1,-1)):
+                    if nqueens[i][j]=='Q':
+                        return False
+                for i,j in zip(range(row-1,-1,-1),range(col+1,n)):
+                    if nqueens[i][j]=='Q':
+                        return False
+                return True
+
+            solvehelper(res,nqueens,0,n)
+            return res
+        
+        return solve(n)
+        
