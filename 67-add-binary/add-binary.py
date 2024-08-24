96552400119c73457d1b62d3d4c86d9718dc466e
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-08-24 18:42:06 +0530

    Time: 26 ms (98.15%) | Memory: 16.5 MB (55.43%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,65 +1,12 @@
 class Solution:
-    def addBinary(self, s1: str, s2: str) -> str:
-        # s1='111'
-        # s2='11'
-        c=0
-        i,j=len(s1)-1,len(s2)-1
-        ans=''
-        while i>-1 and j>-1:
-            ii=s1[i]
-            jj=s2[j]
+    def addBinary(self, a: str, b: str) -> str:
 
-            s=int(ii)+int(jj)+c
-            if s==0 or s==1:
-                ans=ans+str(s)
-                c=0
-            elif s==2:
-                ans=ans+str(0)
-                c=1
-            elif s==3:
-                ans+=str(1)
-                c=1
-            i-=1
-            j-=1
-
-        # print(i,j,ans)
-        while i>-1:
-            ii=s1[i]
-            
-
-            s=int(ii)+c
-            if s==0 or s==1:
-                ans=ans+str(s)
-                c=0
-            elif s==2:
-                ans=ans+str(0)
-                c=1
-            elif s==3:
-                ans+=str(1)
-                c=1
-            i-=1
-        # print(ans)
-        while j>-1:
-            # ii=s1[i]
-            jj=s2[j]
-
-            s=int(jj)+c
-            if s==0 or s==1:
-                ans=ans+str(s)
-                c=0
-            elif s==2:
-                ans=ans+str(0)
-                c=1
-            elif s==3:
-                ans+=str(1)
-                c=1
-            
-            j-=1
-        if c==1:
-            ans=ans+str(c)
-
-        ans=ans[::-1]
-        return ans
-
-
+        num1 = int(a, 2)
+        num2 = int(b, 2)
+        
+        # Add the integers
+        result = num1 + num2
+        
+        # Convert the result back to a binary string and remove the '0b' prefix
+        return bin(result)[2:]
         
