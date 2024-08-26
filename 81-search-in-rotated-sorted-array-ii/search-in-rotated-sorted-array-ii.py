53b177d3f1a62bb0ec8f163747ec7f642e3b5e69
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-08-26 14:46:43 +0530

    Time: 51 ms (66.65%) | Memory: 17.1 MB (29.32%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -2,24 +2,26 @@ class Solution:
     def search(self, nums: List[int], target: int) -> bool:
 
         # # def search(self, nums: List[int], target: int) -> int:
-        # n=len(nums)-1
+        n=len(nums)-1
     
 
-        # l,r=0,n
-        # while l<=r:
-        #     mid=l+(r-l)//2
-        #     midval=nums[mid]
-        #     lval=nums[l]
-        #     rval=nums[r]
-        #     if midval==target:
-        #         return True
-        #     if lval<=target<midval:
-        #         r=mid-1
-        #     elif lval<=midval:
-        #         l=mid+1
-        #     elif midval<target<=rval:
-        #         l=mid+1
-        #     else:
-        #         r=mid-1
-        # return False 
-        return True if target in nums else False
+        l,r=0,n
+        while l<=r:
+            mid=l+(r-l)//2
+            midval=nums[mid]
+            lval=nums[l]
+            rval=nums[r]
+            if midval==target:
+                return True
+            if lval<=target<midval:
+                r=mid-1
+            elif lval<midval:
+                l=mid+1
+            elif lval==midval:
+                l+=1
+            elif midval<target<=rval:
+                l=mid+1
+            else:
+                r=mid-1
+        return False 
+        # return True if target in nums else False
