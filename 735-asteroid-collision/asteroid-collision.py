53f1ef47ff7c87932d04ae971326e34ff3b5c8d1
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-08-28 21:37:38 +0530

    Time: 88 ms (24.25%) | Memory: 17.8 MB (43.43%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,29 +1,30 @@
 class Solution:
     def asteroidCollision(self, l: List[int]) -> List[int]:
-        st = []
-        i = 0
+        st=[]
+        i=0
+        while i<len(l):
 
-        while i < len(l):
             if not st:
                 st.append(l[i])
+                
             else:
-                # If both are of the same sign, just add to stack
-                if st[-1] * l[i] > 0:
+                    # i+=1
+                if st[-1]*l[i]>0:
                     st.append(l[i])
-                elif st[-1] > 0 and l[i] < 0:  # Potential collision
-                    # Resolve the collision
-                    while st and st[-1] > 0 and st[-1] <= abs(l[i]):
-                        if st[-1] == -l[i]:
-                            st.pop()  # Both values cancel out
+                elif st[-1]*l[i]<0:
+                    # print('lkk',l[i],st)
+                    while st and st[-1]>0 and st[-1]<=abs(l[i]):
+                        if st[-1]==-l[i]:
+                            st.pop()
                             break
                         else:
-                            st.pop()  # Remove the positive value
+                            st.pop()
+                    
                     else:
-                        if not st or st[-1] < 0:
-                            st.append(l[i])  # No more collisions or only negatives remain
-                else:
-                    st.append(l[i])  # If no collision, just append
-            i += 1
-
+                        if not st or st[-1]<0:
+                            st.append(l[i])
+                # else:
+                #     st.append(l[i])
+            i+=1
+        # print(st,'llll')
         return st
-        
