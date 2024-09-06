1a89e4136b0362f98dd9f83fe851d6f725a4397a
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-06 17:03:41 +0530

    Time: 586 ms (46.82%) | Memory: 22.7 MB (18.50%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.py
+++ 630-course-schedule-iii/course-schedule-iii.py
@@ -1,26 +1,19 @@
 import heapq
-
 class Solution:
     def scheduleCourse(self, courses: List[List[int]]) -> int:
-        # Sort the courses by their deadline (second element of each sublist)
-        courses.sort(key=lambda x: x[1])
+        courses.sort(key=lambda x:x[1])
+        hp=[]
+        currtime=0
+        for ele in courses:
+            duration,deadline=ele[0],ele[1]
+            heapq.heappush(hp,-duration)
 
-        # A max-heap to keep track of the courses we've taken (use negative durations)
-        max_heap = []
-        current_time = 0
+            currtime+=duration
 
-        for course in courses:
-            duration, last_day = course
+            if currtime>deadline:
+                x=heapq.heappop(hp)
+                currtime+=x
+        return len(hp)
 
-            # Add the current course to the heap
-            heapq.heappush(max_heap, -duration)
-            current_time += duration
-
-            # If the current time exceeds the last day of the current course,
-            # remove the longest duration course from the heap
-            if current_time > last_day:
-                longest_duration = -heapq.heappop(max_heap)
-                current_time -= longest_duration
-
-        # The size of the heap represents the maximum number of courses we can take
-        return len(max_heap)
+        
+        
