06cee5190627bf231c65ecf6af5d5fbe3db83a57
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-06 17:11:12 +0530

    Time: 40 ms (26.91%) | Memory: 16.8 MB (7.13%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,19 +1,21 @@
+from functools import lru_cache
+from typing import List
+
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)==0:return 0
-        if len(nums)==1:return nums[0]
-        if len(nums)==2:return max(nums)
-        def h(nums,dp,i):
-            if i>=len(nums):
-                return 0
-            if i in dp:
-                return dp[i]
-            take=nums[i]+h(nums,dp,i+2)
-            nottake=h(nums,dp,i+1)
-            dp[i]=max(take,nottake)
-            return dp[i]
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums)
         
-        dp1,dp2={},{}
-        a1=nums[1:]
-        a2=nums[:-1]
-        return max(h(a1,dp1,0),h(a2,dp2,0))
+        # Define the helper function with lru_cache for memoization
+        @lru_cache(None)
+        def h(i, nums):
+            if i >= len(nums):
+                return 0
+            take = nums[i] + h(i + 2, nums)
+            nottake = h(i + 1, nums)
+            return max(take, nottake)
+        
+        # Exclude the first house or the last house and calculate the maximum
+        return max(h(0, tuple(nums[1:])), h(0, tuple(nums[:-1])))
+
