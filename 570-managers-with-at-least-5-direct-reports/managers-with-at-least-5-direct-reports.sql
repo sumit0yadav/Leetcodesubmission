366ee3bc37ea9d2e40f9b69cf0ec7547865efddf
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-18 21:26:44 +0530

    Time: 391 ms (25.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,21 @@
+-- # Write your MySQL query statement below
+-- WITH CTE AS (select managerId,count(managerId) as count from Employee
+-- WHERE name is not NULL
+-- GROUP BY managerId
+-- having count(managerId)>4)
+
+-- select e.name as name
+-- FROM CTE c
+-- LEFT JOIN Employee e
+-- ON c.managerId=e.id
+-- WHERE e.name is not NULL
+WITH CTE AS (
+    SELECT managerId, COUNT(managerId) AS count 
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(managerId) > 4
+)
+SELECT e.name AS name
+FROM CTE c
+JOIN Employee e ON c.managerId = e.id;
