0f408f6fdebba0e539e0b4c185a8c4af6337cd5b
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-20 00:11:10 +0530

    Time: 1909 ms (5.01%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,17 +1,13 @@
-SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
-
-FROM Insurance
-
-WHERE tiv_2015 IN 
-
-(SELECT tiv_2015
-FROM Insurance
-GROUP BY tiv_2015
-HAVING COUNT(*)>1)
-AND (lat,lon) IN
-(
-SELECT lat,lon
-FROM Insurance
-GROUP BY lat,lon
-HAVING COUNT(*)=1
+select round(sum(tiv_2016),2) as tiv_2016
+from Insurance I
+where tiv_2015 in (
+    select tiv_2015 from Insurance 
+    group by tiv_2015
+    having count(*)>1
 )
+and (lat,lon) in (
+    select lat,lon from Insurance
+    group by lat,lon
+    having count(*)=1
+)
+
