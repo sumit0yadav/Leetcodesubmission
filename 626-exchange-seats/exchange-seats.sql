6b00df11c58662051801bdc700a02eddadaddb3a
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-19 21:40:22 +0530

    Time: 427 ms (14.07%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,22 +1,10 @@
-/* get all the even numbered rows as odd numbered rows */
-SELECT s1.id - 1 as id, s1.student
-FROM Seat s1
-WHERE s1.id MOD 2 = 0
+# Write your MySQL query statement below
+with cte as (select s1.id-1 as id,s1.student from Seat s1 where s1.id%2=0
+union 
+select s2.id+1 as id,s2.student from Seat s2 where s2.id%2=1
+)
 
-UNION
-
-/* get all the odd numbered rows as even numbered rows */
-SELECT s2.id + 1 as id, s2.student
-FROM Seat s2
-WHERE s2.id MOD 2 = 1 AND s2.id != (SELECT MAX(id) FROM Seat)
-/* Just don't get the last row as we will handle it in the next UNION */
-
-UNION
-
-/* get the last row if odd and don't change the id value */
-SELECT s3.id, s3.student
-FROM Seat s3
-WHERE s3.id MOD 2 = 1 AND s3.id = (SELECT MAX(id) FROM Seat)
-
-/* Order the result by id */
-ORDER BY id ASC;
+select case when (select max(id) from Seat)+1=s3.id then s3.id-1
+else s3.id end as id,s3.student
+from cte as s3
+order by id asc
