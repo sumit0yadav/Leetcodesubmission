538bcf3eae6c144782028224f4b5ece63a0f0572
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-24 17:59:34 +0530

    Time: 86 ms (12.71%) | Memory: 16.7 MB (20.56%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,54 +1,55 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
+
         
-        def validate(board,row,col):
+        def valid(row,col,board):
             for i in range(n):
                 if board[row][i]=='Q':return False
             for i in range(n):
                 if board[i][col]=='Q':return False
+            
             i,j=row,col
             while i>=0 and j>=0:
                 if board[i][j]=='Q':return False
                 i-=1
                 j-=1
             i,j=row,col
+            while i>=0 and j<n:
+                if board[i][j]=='Q':return False
+                i-=1
+                j+=1
+            i,j=row,col
+            while i<n and j>=0:
+                if board[i][j]=='Q':return False
+                i+=1
+                j-=1
+            i,j=row,col
             while i<n and j<n:
                 if board[i][j]=='Q':return False
                 i+=1
                 j+=1
-            i,j=row,col
-            while i>=0 and j<n:
-                if board[i][j]=='Q':return False
-                i=i-1
-                j+=1
-            i,j=row,col
-
-            while i<n and j>=0:
-                if board[i][j]=='Q':return False
-                i=i+1
-                j-=1
             return True
 
-        
+
+
+
         def dfs(col,board,res):
             if col==n:
                 res[0]+=1
                 return
+            
             for row in range(n):
-                if validate(board,row,col):
+                if valid(row,col,board):
                     board[row][col]='Q'
                     dfs(col+1,board,res)
                     board[row][col]='.'
-            return
+
 
 
         board=[['.']*n for _ in range(n)]
         res=[0]
-        # for col in range(n):
         dfs(0,board,res)
         return res[0]
-
-
-
+        
 
         
