e4dae2dd8e10859d070a75cc239db25040cc700f
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-29 13:52:35 +0530

    Time: 886 ms (7.83%) | Memory: 333.4 MB (9.72%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -1,43 +1,55 @@
 class Solution:
-    def shortestCommonSupersequence(self, text1: str, text2: str) -> str:
-        def h(i,j,t1,t2,dp,st):
-            if i<0 or j<0:return 0,''
+    def shortestCommonSupersequence(self, t1: str, t2: str) -> str:
+
+        def h(i,j,t1,t2,dp):
+            if i<0 or j<0:
+                return 0,''
+            
             if (i,j) in dp:
                 return dp[(i,j)]
+
             if t1[i]==t2[j]:
-                
-                l,st=h(i-1,j-1,t1,t2,dp,st)
+                l,s= h(i-1,j-1,t1,t2,dp)
                 l=l+1
-                st=st+t1[i]
-                
-                
+                s=s+t1[i]
             else:
-                l1,s1=h(i-1,j,t1,t2,dp,st)
-                l2,s2=h(i,j-1,t1,t2,dp,st)
+
+                l1,s1=h(i-1,j,t1,t2,dp)
+                l2,s2=h(i,j-1,t1,t2,dp)
 
                 if l1>l2:
+                    
+                    
                     l=l1
-                    st=s1
+                    s=s1
                 else:
                     l=l2
-                    st=s2
+                   
+                    s=s2
+                
+                
+            dp[(i,j)]=(l,s)
+            return l,s
 
-            dp[(i,j)]=(l,st)
-            return (l,st)
+        n1,n2=len(t1)-1,len(t2)-1
         dp={}
-        n1=len(text1)-1
-        n2=len(text2)-1
-        st=''
-        _,lcs= h(n1,n2,text1,text2,dp,st)
+        _,lcs=h(n1,n2,t1,t2,dp)
 
-        res, i, j = "", 0, 0
-        for c in lcs:
-            while text1[i] != c:
-                res += text1[i]
-                i += 1
-            while text2[j] != c:
-                res += text2[j]
-                j += 1
-            res += c
-            i, j = i + 1, j + 1
-        return res + text1[i:] + text2[j:]
+        res,i,j='',0,0
+        for char in lcs:
+            while t1[i]!=char:
+                res+=t1[i]
+                i+=1
+            while t2[j]!=char:
+                res+=t2[j]
+                j+=1
+            res+=char
+            i+=1
+            j+=1
+        ans=res+t1[i:]+t2[j:]
+        return ans
+            
+
+
+
+        
