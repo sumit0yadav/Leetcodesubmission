79968f733c752625f8241496f27a2a6e68680250
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-09-30 19:26:43 +0530

    Time: 1222 ms (17.98%) | Memory: 32.8 MB (35.04%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.py
+++ 239-sliding-window-maximum/sliding-window-maximum.py
@@ -1,39 +1,33 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        if k == 1:
-            return nums
-        if k >= len(nums):
-            return [max(nums)]
+        if k==1:return nums
+        if k>=len(nums):return [max(nums)]
 
-        stack = []  # Using stack instead of deque
-        ans = []
-        maxv = -1e9
-        maxind = -1
-
+        stack=[]
+        maxval=-1e9
+        maxind=-1
+        ans=[]
         for i in range(len(nums)):
 
-            # Remove the elements which are out of this window
-            if i - maxind >= k:
-                stack.pop(0)  # Remove the first element since it's out of the window
-                maxv = stack[0][0]
-                maxind = stack[0][1]
+            if stack and maxind+k<=i:
+                x,y=stack.pop(0)
+                maxind=stack[0][1]
+                maxval=stack[0][0]
 
-            if nums[i] >= maxv:
-                # Clear the stack when a new maximum is found
-                stack = []
-                maxv = nums[i]
-                maxind = i
-                stack.append((nums[i], i))
+            if maxval<=nums[i]:
+                maxind=i
+                maxval=nums[i]
+                stack=[]
+                stack.append([maxval,maxind])
             else:
-                # Maintain the stack by popping smaller elements from the end
-                while stack and nums[i] >= stack[-1][0]:
+                while stack and nums[i]>=stack[-1][0]:
                     stack.pop()
-
-                # Add the current element to the stack
-                stack.append((nums[i], i))
-
-            # Once the window is full, start recording the maximums
-            if i >= k - 1:
+                stack.append([nums[i],i])
+                
+            # print(stack)
+            if stack and i>=k-1:
                 ans.append(stack[0][0])
-
         return ans
+
+
+        
