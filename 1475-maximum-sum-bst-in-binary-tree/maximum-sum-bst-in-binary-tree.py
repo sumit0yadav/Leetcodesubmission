07454e423b392c277e081aa14d11e30460e1e6b4
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-01 18:48:30 +0530

    Time: 333 ms (8.73%) | Memory: 37.8 MB (6.66%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.py
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.py
@@ -6,21 +6,39 @@
 #         self.right = right
 class Solution:
     def maxSumBST(self, root: Optional[TreeNode]) -> int:
-        curr=root
-        self.maxsum=0
-        def h(curr):
-            if not curr:
-                return True,0,1e9,-1e9
-            l,lsum,lmin,lmax=h(curr.left)
-            r,rsum,rmin,rmax=h(curr.right)
 
-            if l and r and(lmax<curr.val<rmin):
-                self.maxsum=max(self.maxsum,curr.val+lsum+rsum)
-                return True,curr.val+lsum+rsum,min(lmin,curr.val),max(rmax,curr.val)
+        def h(node):
+            if not node.left and not node.right:
+                # print(True,node.val,node.val,node.val)
+                return True,node.val,node.val,node.val,node.val
+
+            if node.left:
+                isbst_left,min_left,max_left,sum_left,maxsum_left=h(node.left)
             else:
-                self.maxsum=max(self.maxsum,lsum,rsum)
+                isbst_left,min_left,max_left,sum_left,maxsum_left=True,1e9,-1e9,0,0
 
-                return False, max(lsum,rsum),1e9,-1e9
-        x=h(root)
-        return self.maxsum
+            if node.right:
+                isbst_right,min_right,max_right,sum_right,maxsum_right=h(node.right)
+            else:
+                isbst_right,min_right,max_right,sum_right,maxsum_right=True,1e9,-1e9,0,0
+
+            isbst= isbst_left and isbst_right and (max_left<node.val<min_right)
+            if isbst:
+                # if node.val==2:
+                #     print(min_right,'val',node.right.val)
+                #     print(True,node.val,min_left,max_right,sum_left,sum_right,sum_left+sum_right+node.val)
+                x=max(sum_left+sum_right+node.val,maxsum_left,maxsum_right)
+                print('True',node.val,x)
+                
+                return True,min(node.val,min_left),max(max_right,node.val),sum_left+sum_right+node.val,x
+            else:
+                y=max(maxsum_left,maxsum_right)
+                print('False',node.val,y)
+                
+                return False,1e9,-1e9,max(sum_left,sum_right),y
+
+        a,b,c,d,e= h(root)
+        # print(a,d,e)
+        return max(0,e)
+            
         
