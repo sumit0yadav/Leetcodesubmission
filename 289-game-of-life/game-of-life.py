04ea6d5747a54f027c0c005e2a4199d46c04e538
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-02 20:47:51 +0530

    Time: 43 ms (15.98%) | Memory: 16.5 MB (88.90%) - LeetSync

--- 289-game-of-life/game-of-life.py
+++ 289-game-of-life/game-of-life.py
@@ -1,8 +1,8 @@
 class Solution:
     import copy
-    def gameOfLife(self, board: List[List[int]]) -> None:
+    def gameOfLife(self, grid: List[List[int]]) -> None:
         """
-        Do not return anything, modify board in-place instead.
+        Do not return anything, modify grid in-place instead.
         """
         def count(grid,i,j):
             ch=0
@@ -10,19 +10,27 @@ class Solution:
 
             for dx,dy in dir:
                 if 0<=i+dx<len(grid) and 0<=j+dy<len(grid[0]):
-                    ch+=grid[i+dx][j+dy]
+                    ch+=grid[i+dx][j+dy][0]
             return ch
-
-        grid=copy.deepcopy(board)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]==1 and count(grid,i,j)<2:
-                    board[i][j]=0
-                elif grid[i][j]==1 and (2<=count(grid,i,j)<=3):
-                    board[i][j]=1
-                elif grid[i][j]==1 and count(grid,i,j)>3:
-                    board[i][j]=0
-                elif grid[i][j]==0 and count(grid,i,j)==3:
-                    board[i][j]=1
+                x=grid[i][j]
+                grid[i][j]=[x,x]
+
+        # grid=copy.deepcopy(grid)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j][0]==1 and count(grid,i,j)<2:
+                    grid[i][j][1]=0
+                elif grid[i][j][0]==1 and (2<=count(grid,i,j)<=3):
+                    grid[i][j][1]=1
+                elif grid[i][j][0]==1 and count(grid,i,j)>3:
+                    grid[i][j][1]=0
+                elif grid[i][j][0]==0 and count(grid,i,j)==3:
+                    grid[i][j][1]=1
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                x=grid[i][j]
+                grid[i][j]=x[1]
 
         
