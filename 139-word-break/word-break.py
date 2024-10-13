d9256ed00a516781d036966997c569a4a0915987
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-13 18:47:38 +0530

    Time: 41 ms (51.29%) | Memory: 17.1 MB (8.08%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,32 +1,16 @@
-from typing import List
-
 class Solution:
-    # from typing import List
-
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        n = len(s)
-        t = [None] * n  # Memoization array
-        
-        def solve(idx: int) -> bool:
-            if idx == n:
-                return True
-            
-            if t[idx] is not None:
-                return t[idx]
-            
-            for endIdx in range(idx + 1, n + 1):
-                split = s[idx:endIdx]
-                
-                if split in wordDict and solve(endIdx):
-                    t[idx] = True
-                    return True
-            
-            t[idx] = False
+        wordset=set(wordDict)
+        @cache
+        def h(ind):
+            # if ind>=len(s):return False
+            if ind==len(s):return True
+            for i in range(ind+1,len(s)+1):
+                tmp=s[ind:i]
+                if tmp in wordset:
+                    
+                    if h(i):
+                        return True
             return False
+        return h(0)
         
-        return solve(0)
-
-    # # Example usage:
-    # print(wordBreak("leetcode", ["leet", "code"]))  # Output: True
-    # print(wordBreak("applepenapple", ["apple", "pen"]))  # Output: True
-    # print(wordBreak("catsandog", ["cats", "dog", "sand", "and", "cat"]))  # Output: False
