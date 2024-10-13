c301b214e79cdf6022d6e32fa72227a901165a8f
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-13 18:17:57 +0530

    Time: 1396 ms (8.02%) | Memory: 443.2 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,27 +1,22 @@
-from typing import List
-
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
+    def coinChange(self, l: List[int], amt: int) -> int:
+        l.sort()
+        @cache
+        def h(ind,s):
+            if ind<0:return 1e9
+            if s<0:return 1e9
+            if s==0:return 0
+            take,nottake=1e9,1e9
+            if s>=l[ind]:
+                take=1+h(ind,s-l[ind])
+            nottake=h(ind-1,s)
+            ans=min(take,nottake)
+            return ans
+        x= h(len(l)-1,amt)
+        if x==1e9:return -1
+        return x
+            
 
-        def h(nums, dp, i, sum):
-            if sum == 0:
-                return 0  # No coins needed
-            if sum < 0 or i == len(nums):
-                return float('inf')  # Invalid path
-            
-            # Check if the result for this state is already computed
-            if (i, sum) in dp:
-                return dp[(i, sum)]
-            
-            # Include the current coin
-            take = 1 + h(nums, dp, i, sum - nums[i])
-            # Exclude the current coin
-            nottake = h(nums, dp, i + 1, sum)
-            
-            # Store the result in dp dictionary and return
-            dp[(i, sum)] = min(take, nottake)
-            return dp[(i, sum)]
 
-        dp = {}
-        result = h(coins, dp, 0, amount)
-        return result if result != float('inf') else -1  # Return -1 if no solution
+
+        
