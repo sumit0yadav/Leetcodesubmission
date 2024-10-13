d2e033a3e384f6ea1f0e3546ac12fa9911d5bb78
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-13 13:55:52 +0530

    Time: 550 ms (30.61%) | Memory: 24.7 MB (16.60%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,41 +1,33 @@
 from collections import deque
 class Solution:
-    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        def reverse(adj):
-  
-            reversed_adj = [[] for _ in range(len(adj))]
-            
-            for u in range(len(adj)):
-                for v in adj[u]:
-                    reversed_adj[v].append(u)
-                    
-            return reversed_adj
-        
-        adj=reverse(graph)
-        ind=[0]*len(adj)
-        for i in range(len(adj)):
-            for ele in adj[i]:
-                ind[ele]+=1
-        q=deque()
-        for i in range(len(ind)):
-            if ind[i]==0:
+    def eventualSafeNodes(self, G: List[List[int]]) -> List[int]:
+        from collections import deque, defaultdict
+
+    # def eventualSafeNodes(G):
+        N = len(G)
+        R = defaultdict(list)  # Reverse graph
+        outdegree = [0] * N
+        safe = [0] * N
+        ans = []
+        q = deque()
+
+        for i in range(N):
+            for v in G[i]:
+                R[v].append(i)
+            outdegree[i] = len(G[i])
+            if outdegree[i] == 0:
                 q.append(i)
         
-        st=set()
-        vert=set(range(len(adj)+1))
         while q:
+            u = q.popleft()
+            safe[u] = 1
+            for v in R[u]:
+                outdegree[v] -= 1
+                if outdegree[v] == 0:
+                    q.append(v)
 
-            node = q.popleft()
-            st.add(node)
-            for nei in adj[node]:
-                ind[nei]-=1
-                if ind[nei]==0:
-                    q.append(nei)
-        ans=list(st)
-        ans.sort()
+        for i in range(N):
+            if safe[i]:
+                ans.append(i)
+        
         return ans
-            
-        
-
-        
-        
