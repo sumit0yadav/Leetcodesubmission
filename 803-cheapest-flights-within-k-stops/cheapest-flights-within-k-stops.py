8620d8655f170740481c707f0b7918f3768f2ee9
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-13 16:54:00 +0530

    Time: 87 ms (92.26%) | Memory: 18 MB (35.68%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.py
@@ -1,20 +1,25 @@
-from collections import deque
 class Solution:
+    from collections import deque
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        
         adj=[[] for _ in range(n)]
-        for flight in flights:
-            adj[flight[0]].append((flight[1],flight[2]))
-        q= deque([(0,src,0)])
-        dist=[float('inf')]*n
+        for ele in flights:
+            adj[ele[0]].append([ele[1],ele[2]])
+        q=deque()
+        q.append((0,src,0))
+        dist=[1e9]*n
         dist[src]=0
+
         while q:
             stops,node,cost=q.popleft()
-            if stops>k:
-                continue
-            
-            for adjnode,edw in adj[node]:
-                if cost+edw<dist[adjnode]:
-                    dist[adjnode]=cost+edw
-                    q.append((stops+1,adjnode,edw+cost))
-        return -1 if dist[dst]==float('inf') else dist[dst]        
+            if stops>k:continue
+            for nei,neicost in adj[node]:
+                if cost+neicost<dist[nei]:
+                    dist[nei]=cost+neicost
+                    q.append((stops+1,nei,cost+neicost))
+        if dist[dst]==1e9:return -1
+        return dist[dst]
+
+
+
+
+        
