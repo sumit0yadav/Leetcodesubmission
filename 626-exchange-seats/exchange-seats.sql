0e3011ade22ba09a65da6cd9b3a116221f7feb9c
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-19 19:16:47 +0530

    Time: 505 ms (8.31%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,10 +1,9 @@
-# Write your MySQL query statement below
-with cte as (select s1.id-1 as id,s1.student from Seat s1 where s1.id%2=0
-union 
-select s2.id+1 as id,s2.student from Seat s2 where s2.id%2=1
+
+with cte as (select s1.id-1 as id,s1.student as student from Seat s1 where s1.id%2=0
+union
+select s1.id+1 as id,s1.student as student from Seat s1 where s1.id%2=1 
+order by id asc
 )
 
-select case when (select max(id) from Seat)+1=s3.id then s3.id-1
-else s3.id end as id,s3.student
-from cte as s3
-order by id asc
+select case when s.id=(select max(id) from Seat)+1 then s.id-1 else s.id end as id ,s.student
+from cte s
