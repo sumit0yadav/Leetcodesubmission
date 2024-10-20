b812b558cf8a1d7c6232a65cc15266e0f1b2b87a
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-20 23:44:40 +0530

    Time: 11 ms (99.05%) | Memory: 17.1 MB (68.47%) - LeetSync

--- 684-redundant-connection/redundant-connection.py
+++ 684-redundant-connection/redundant-connection.py
@@ -1,20 +1,22 @@
 class Solution:
+    from collections import defaultdict
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        self.already_connected = defaultdict(list)
+        def already(x,y,adjj,viss):
+            if x==y:return True
+
+            for x_ in adjj[x]:
+                if x_ not in vis:
+                    vis.add(x_)
+                    if already(x_,y,adjj,viss):
+                        return True
+            return False
+        adj=defaultdict(list)
         for edge in edges:
-            self.visited = defaultdict(bool)
-            x, y = edge[0], edge[1]
-            if self.is_already_connected(x, y):
+            vis=set()
+            x,y=edge[0],edge[1]
+            if already(x,y,adj,vis):
                 return edge
-            self.already_connected[x].append(y)
-            self.already_connected[y].append(x)
-            
-    def is_already_connected(self, x, y):
-        if x == y:
-            return True
-        for x_adjacent in self.already_connected[x]:
-            if not self.visited[x_adjacent]:
-                self.visited[x_adjacent] = True
-                if self.is_already_connected(x_adjacent, y):
-                    return True
-        return False
+            adj[x].append(y)
+            adj[y].append(x)
+        
+        
