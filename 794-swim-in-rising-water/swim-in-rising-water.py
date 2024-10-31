222bd83943380107527fcc6efd69f847c06dc537
Sumit Yadav <104365639+sumit0yadav@users.noreply.github.com>
2024-10-31 23:55:39 +0530

    Time: 15 ms (99.56%) | Memory: 17 MB (91.24%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.py
+++ 794-swim-in-rising-water/swim-in-rising-water.py
@@ -6,32 +6,26 @@ moves = [[1, 0], [0, 1], [-1, 0], [0, -1]]
 
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
-        # Initialize variables
-        N = len(grid) - 1  # grid size - 1 for boundary checking
-        i, j = 0, 0  # starting position
-        pq = []  # priority queue (min-heap)
-        ans = grid[0][0]  # minimum time to reach (0, 0)
+        
+        N = len(grid) - 1  
+        i, j = 0, 0  
+        pq = []  
+        ans = grid[0][0] 
 
-        # Main loop to traverse the grid until reaching the bottom-right corner
-        while i != N or j != N:
-            # Explore all four possible directions
+        while i < N or j < N:
             for a, b in moves:
                 ia, jb = i + a, j + b  # new position
 
-                # Continue if the new position is out of bounds or already visited
                 if ia < 0 or ia > N or jb < 0 or jb > N or grid[ia][jb] > 2500:
                     continue
 
-                # Push new cell to the priority queue with its elevation as the priority
+                
                 heappush(pq, (grid[ia][jb], ia, jb))
 
-                # Mark cell as visited by setting a high value
                 grid[ia][jb] = 3000
 
-            # Pop the next cell with the lowest elevation from the priority queue
             elevation, i, j = heappop(pq)
 
-            # Update the answer with the maximum elevation seen so far
             ans = max(ans, elevation)
 
         return ans
